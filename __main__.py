1809fd57b07c1bf0fa6f667002c1ee5dadf70b0f
Jiahao Li <reg@ljh.me>
2019-07-19 19:58:21 -0400

    Implements CLI interface for channel monitoring

--- __main__.py
+++ __main__.py
@@ -1,20 +1,33 @@
 import peewee
+import argparse
 
-from youtube_monitor import ChannelMonitor, VideoMonitor
+from youtube_monitor import ChannelMonitor
 from youtube_monitor.models import initialize_database
 
 DATABASE_PATH = "data.db"
 
 
 def main():
-    db = peewee.SqliteDatabase(DATABASE_PATH)
+    parser = argparse.ArgumentParser(
+        description="Gather information of a YouTube channel and its newest videos."
+    )
+    parser.add_argument(
+        "db_path",
+        type=str,
+        help="path to an SQLite database to save results into.",
+    )
+    parser.add_argument(
+        "channel_id",
+        type=str,
+        help="ID of the channel to gather information of.",
+    )
+
+    args = parser.parse_args()
+
+    db = peewee.SqliteDatabase(args.db_path)
     initialize_database(db)
 
-    test_video_id = "FiyJf-8lMmo"
-    test_channel_id = 'UCdXqCN_HtF_RjlsHzDSnJIQ'
-    monitor = ChannelMonitor(test_channel_id)
-
-    for reading in monitor.get_readings():
+    for reading in ChannelMonitor(args.channel_id).get_readings():
         reading.save()
 
 
